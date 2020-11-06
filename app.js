acos-server/acos-server

commit c39157bc07de4972d441ce100f9ee379bfb5500c

Implement hot reloading with livereload+nodemon

app.js
@@ -19,6 +19,9 @@ var archiver = require('archiver');
 var crypto = require('crypto');
 var _ = require('lodash');
 
+
+const publicDirectory = path.join(__dirname, 'public');
+
 var app = express();
 
 if (app.get('env') !== 'development') {
@@ -43,7 +46,28 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(compression());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(publicDirectory));
+
+if (app.get('env') === 'development') {
+  const livereload = require('livereload');
+  const connectLiveReload = require('connect-livereload');
+
+  // livereload configuration
+  let liveReloadServer = livereload.createServer();
+  liveReloadServer.watch(publicDirectory);
+  liveReloadServer.server.once("connection", () => {
+    setTimeout( () =>{
+    liveReloadServer.refresh("/");
+    }, 100);
+  })
+
+  app.use(connectLiveReload({
+    port: config.connectLiveReloadPort
+  }));
+}
+
+
+
 
 // ********************************************************************************
 // ACOS code begins
@@ -624,7 +648,6 @@ handlers.loggers.logstore.handleEvent = function(event, payload, req, res, proto
 
 // Dummy logger for debugging purposes
 if (app.get('env') === 'development') {
-
   handlers.loggers.dummy = {};
   handlers.loggers.dummy.handleEvent = function(event, payload, req, res, protocolData) {
     console.log('[ACOS Server] ' + 'INFO:'.green + ' Event received => event: %s, payload: %j , protocol data: %j',

config.js
@@ -22,6 +22,10 @@ var CONFIG = {
     // the package name must start with 'acos-'.
     autoDiscovery: true,
 
+
+    // Default port used by the connect-livereload package
+    connectLiveReloadPort: 35729,
+
     // Insert all the installed package names here to use them
     // These are ignored if autoDiscovery is true!
     installedPackages: [

commit d81a3441f0bde73d199e165e3a6164d74619b0b5

Fix error introduced in c39157b
The config module was called before it was imported.

app.js
@@ -18,6 +18,7 @@ var nunjucks = require('nunjucks');
 var archiver = require('archiver');
 var crypto = require('crypto');
 var _ = require('lodash');
+var config = require('./config');
 
 
 const publicDirectory = path.join(__dirname, 'public');
@@ -76,7 +77,6 @@ if (app.get('env') === 'development') {
 // ********************************************************************************
 // Initialization
 
-var config = require('./config');
 
 console.log('[ACOS Server]' + ' INFO: '.green + 'Starting ACOS at ' + new Date() + '\n');
 